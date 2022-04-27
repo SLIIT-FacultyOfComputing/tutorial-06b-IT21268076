d3c92e8e615ba358fad96d5e17ace9fe6abb2791
IT21268076 <99285844+IT21268076@users.noreply.github.com>
2022-04-27 19:30:50 +0000

    Complete

--- Box.cpp
+++ Box.cpp
@@ -1,7 +1,25 @@
 #include "Box.h"
-
+  void Box::setLength(double l){
+    length = l;
+  }
+  void Box::setWidth(double w){
+    width = w;
+  }
+  void Box::setHeight(double h){
+    height = h;
+  }
+  int Box::getLength(){
+    return length;
+  }
+int Box::getWidth(){
+  return width;
+}
+int Box::getHeight(){
+  return height;
+}
 // Implement setters and getters
 
 // Implemenet the calcVolume() unction
 int Box::calcVolume() {
+  return length*width*height;
 }
--- Box.h
+++ Box.h
@@ -6,6 +6,11 @@ class Box {
     public:
        // write prototypes of setters for length, width and height
        // write prototypes of getters for length, width and height
- 
-       int calcVolume();
+      void setLength(double l);
+      void setWidth(double w);
+      void setHeight(double h);
+      int getLength();
+      int getWidth();
+      int getHeight();
+      int calcVolume();
 };
